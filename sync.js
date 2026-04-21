e1a502c2a420fa85f5a22166c6de56ea92211fd6
Wesley Wanga <wesleyjwanga@gmail.com>
2026-04-21 19:50:53 +0300

    update JS file

---
+++ sync.js
@@ -0,0 +1,14 @@
+// console.log("Task1");
+// console.log("Task2")
+// console.log("Task3")
+
+// console.log("Start");
+
+// function longTask(){
+//     const start= Date.now();
+//     while (Date.now()-start<3000){} // blocks for 3 seconds
+//     console.log("Long task done");
+// }
+// longTask()
+
+// console.log("End") 
